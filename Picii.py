bb2ab72b7c428e481773fb5493f7b79d913338ec
Alex <alex@alexjshepler.io>
2024-03-20 17:25:53 -0700

    Added functionality to resize original image

--- Picii.py
+++ Picii.py
@@ -1,7 +1,16 @@
 import PIL.Image
 
+# ASCII Characters used ot build the output
+ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']
+    
+# Resize image
+def resizeImage(image, newWidth=100):
+    width, height = image.size # Get image size
+    ratio = height / width # Get image ratio
+    newHeight = int(newWidth * ratio) # Set new height based on ratio and new width
+    return image.resize((newWidth, newHeight)) # Return new image
+    
 def main():
-
     # Get an image from user input
     path = input("Enter a valid pathname to an image:\n")
     try:
