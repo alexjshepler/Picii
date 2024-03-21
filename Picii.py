db0665a4ef0100efb4e6cdcdd2b22bcfc5705d09
Alex <alex@alexjshepler.io>
2024-03-20 19:06:52 -0700

    Finished Project and fixed bugs

--- Picii.py
+++ Picii.py
@@ -8,7 +8,7 @@ def resizeImage(image, newWidth=100):
     width, height = image.size # Get image size
     ratio = height / width # Get image ratio
     newHeight = int(newWidth * ratio) # Set new height based on ratio and new width
-    return image.resize((newWidth, newHeight)) # Return new image
+    return (image.resize((newWidth, newHeight))) # Return new image
     
 
 # Convert to grayscale
@@ -18,12 +18,27 @@ def convertToGrayscale(image):
 # Convert pixels to ASCII
 def pixelsToAscii(image):
     img = image.getdata()
-    return "".join([ASCII_CHARS[pixel//25] for i in img])
+    return "".join([ASCII_CHARS[i//25] for i in img])
 
-def main():
+def main(newWidth=100):
     # Get an image from user input
     path = input("Enter a valid pathname to an image:\n")
     try:
         image = PIL.Image.open(path)
     except:
         print(path, " Is not a valid pathname to an image")
+        
+    # Convert Image to ASCII
+    newImageData = pixelsToAscii(convertToGrayscale(resizeImage(image)))
+    
+    # Format Image
+    pixelCount = len(newImageData)
+    asciiImage = '\n'.join(newImageData[i:(i+newWidth)] for i in range(0, pixelCount, newWidth))
+    
+    print(asciiImage)
+    
+    # Save Image
+    with open("asciiImage.txt", "w") as f:
+        f.write(asciiImage)
+    
+main()
